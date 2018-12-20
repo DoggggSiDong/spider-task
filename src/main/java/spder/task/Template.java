e501785b199d21c05cd21472b3dd3f9c91cdd31f
DoggggSiDong <sidong.zj@foxmail.com>
2018-12-20 18:07:16 +0800

    spider-task

---
+++ src/main/java/spder/task/Template.java
@@ -0,0 +1,19 @@
+package spder.task;
+
+import us.codecraft.webmagic.Spider;
+
+public abstract class Template implements Runnable {
+    private Task task;
+    private Spider spider;
+    public Template(Task task){
+        this.task = task;
+    }
+
+    public Task getTask(){
+        return task;
+    }
+
+    public void runTask(){
+        spider.run();
+    }
+}
